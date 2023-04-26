2e766ff0b001f4f80e68f484680090b88ef3cf11
Alex Acebo <44811138+aacebo@users.noreply.github.com>
2023-04-25 19:56:46 -0700

    add set

---
+++ set.go
@@ -0,0 +1,45 @@
+package std
+
+type Set[T comparable] map[T]bool
+
+func NewSet[T comparable](items ...T) Set[T] {
+	self := Set[T]{}
+
+	for _, v := range items {
+		self[v] = true
+	}
+
+	return self
+}
+
+func (self Set[T]) Size() int {
+	return len(self)
+}
+
+func (self Set[T]) Empty() bool {
+	return self.Size() == 0
+}
+
+func (self Set[T]) Has(items ...T) bool {
+	for _, v := range items {
+		if _, ok := self[v]; !ok {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (self Set[T]) Add(items ...T) {
+	for _, v := range items {
+		self[v] = true
+	}
+}
+
+func (self Set[T]) Delete(items ...T) {
+	for _, v := range items {
+		if _, ok := self[v]; ok {
+			delete(self, v)
+		}
+	}
+}
---
+++ set_test.go
@@ -0,0 +1,73 @@
+package std
+
+import "testing"
+
+func TestSet(t *testing.T) {
+	t.Run("Size", func(t *testing.T) {
+		t.Run("should have correct size", func(t *testing.T) {
+			s := NewSet(1, 2, 3, 4, 5, 2)
+
+			if s.Size() != 5 {
+				t.Errorf("should have size %d", 5)
+			}
+		})
+	})
+
+	t.Run("Empty", func(t *testing.T) {
+		t.Run("should be empty", func(t *testing.T) {
+			s := NewSet[int]()
+
+			if !s.Empty() {
+				t.Errorf("should be empty")
+			}
+		})
+
+		t.Run("should not be empty", func(t *testing.T) {
+			s := NewSet(0, 1, 2)
+
+			if s.Empty() {
+				t.Error("should not be empty")
+			}
+		})
+	})
+
+	t.Run("Has", func(t *testing.T) {
+		t.Run("should have elements", func(t *testing.T) {
+			s := NewSet(0, 1, 2)
+
+			if !s.Has(0, 1, 2) {
+				t.Errorf("should have elements")
+			}
+		})
+
+		t.Run("should not have element", func(t *testing.T) {
+			s := NewSet(0, 1, 2)
+
+			if s.Has(0, 2, 7) {
+				t.Errorf("should not have 7")
+			}
+		})
+	})
+
+	t.Run("Add", func(t *testing.T) {
+		t.Run("should add new elements", func(t *testing.T) {
+			s := NewSet(0, 1, 2)
+			s.Add(1, 2, 3)
+
+			if !s.Has(0, 1, 2, 3) {
+				t.Errorf("should have 0, 1, 2, 3")
+			}
+		})
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+		t.Run("should delete elements", func(t *testing.T) {
+			s := NewSet(0, 1, 2)
+			s.Delete(1, 2, 3)
+
+			if s.Has(1, 2) {
+				t.Errorf("should not have 1, 2")
+			}
+		})
+	})
+}
