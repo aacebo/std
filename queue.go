4b075bf5842daec4aa0eb4d649756d2c6b0d66d6
Alex Acebo <44811138+aacebo@users.noreply.github.com>
2023-04-25 23:20:04 -0700

    add queue

---
+++ queue.go
@@ -0,0 +1,36 @@
+package std
+
+type Queue[T any] []T
+
+func NewQueue[T any](items ...T) Queue[T] {
+	self := Queue[T]{}
+	self = append(self, items...)
+	return self
+}
+
+func (self Queue[T]) Size() int {
+	return len(self)
+}
+
+func (self Queue[T]) Empty() bool {
+	return self.Size() == 0
+}
+
+func (self Queue[T]) Top() T {
+	return self[0]
+}
+
+func (self Queue[T]) Bottom() T {
+	return self[self.Size()-1]
+}
+
+func (self *Queue[T]) Push(items ...T) {
+	*self = append(*self, items...)
+}
+
+func (self *Queue[T]) Pop() T {
+	v := self.Top()
+	arr := *self
+	*self = arr[1:]
+	return v
+}
---
+++ queue_test.go
@@ -0,0 +1,91 @@
+package std
+
+import "testing"
+
+func TestQueue(t *testing.T) {
+	t.Run("Size", func(t *testing.T) {
+		t.Run("should have correct size", func(t *testing.T) {
+			q := NewQueue(0, 1, 2)
+
+			if q.Size() != 3 {
+				t.Errorf("should have size of 3")
+			}
+		})
+	})
+
+	t.Run("Empty", func(t *testing.T) {
+		t.Run("should be empty", func(t *testing.T) {
+			q := NewQueue[int]()
+
+			if !q.Empty() {
+				t.Errorf("should be empty")
+			}
+		})
+
+		t.Run("should not be empty", func(t *testing.T) {
+			q := NewQueue(0, 1, 2)
+
+			if q.Empty() {
+				t.Errorf("should not be empty")
+			}
+		})
+	})
+
+	t.Run("Top", func(t *testing.T) {
+		t.Run("should be first element", func(t *testing.T) {
+			q := NewQueue(0, 1, 2)
+
+			if q.Top() != 0 {
+				t.Errorf("should have top of 0")
+			}
+		})
+	})
+
+	t.Run("Bottom", func(t *testing.T) {
+		t.Run("should be last element", func(t *testing.T) {
+			q := NewQueue(0, 1, 2)
+
+			if q.Bottom() != 2 {
+				t.Errorf("should have bottom of 2")
+			}
+		})
+	})
+
+	t.Run("Push", func(t *testing.T) {
+		t.Run("should add new element to back", func(t *testing.T) {
+			q := NewQueue(0, 1, 2)
+
+			if q.Bottom() != 2 {
+				t.Errorf("should have bottom of 2")
+			}
+
+			q.Push(5)
+
+			if q.Bottom() != 5 {
+				t.Errorf("should have bottom of 5")
+			}
+
+			if q.Size() != 4 {
+				t.Errorf("should have size of 4")
+			}
+		})
+	})
+
+	t.Run("Pop", func(t *testing.T) {
+		t.Run("should remove element from front", func(t *testing.T) {
+			q := NewQueue(0, 1, 2)
+
+			if q.Top() != 0 {
+				t.Errorf("should have top of 0")
+			}
+
+			if q.Pop() != 0 {
+				t.Errorf("should have popped off 0")
+			}
+
+			if q.Size() != 2 {
+				t.Errorf("should have size of 2")
+			}
+		})
+	})
+}
